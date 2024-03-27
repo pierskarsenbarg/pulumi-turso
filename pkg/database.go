ee49d26f533fc765e928c8fa7172c64750e73ae0
Piers Karsenbarg <piers.karsenbarg@gmail.com>
2024-03-27 22:32:46 +0000

    Added preview check to create database

--- pkg/database.go
+++ pkg/database.go
@@ -1,6 +1,8 @@
 package pkg
 
-import p "github.com/pulumi/pulumi-go-provider"
+import (
+	p "github.com/pulumi/pulumi-go-provider"
+)
 
 type Database struct{}
 
@@ -9,5 +11,11 @@ type DatabaseArgs struct{}
 type DatabaseState struct{}
 
 func (o *Database) Create(ctx p.Context, name string, input DatabaseArgs, preview bool) (id string, output DatabaseState, err error) {
+	// config := infer.GetConfig[Config](ctx)
+
+	if preview {
+		return "", DatabaseState{}, nil
+	}
+
 	return "", DatabaseState{}, nil
 }
