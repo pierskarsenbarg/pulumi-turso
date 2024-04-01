1fde7b98085096008e2afd0dabeff88747919967
Piers Karsenbarg <piers.karsenbarg@gmail.com>
2024-04-01 20:47:48 +0100

    Updatinc ci scripts

--- ci/check-working-dir.ts
+++ ci/check-working-dir.ts
@@ -4,6 +4,8 @@ await $`git update-index -q --refresh`;
 
 const { stdout, stderr, exitCode } = await $`git diff-files`.quiet().nothrow();
 
+console.log(`exit code: ${exitCode}`);
+
 if (exitCode !== 0) {
     console.log(`Working directory dirty.`);
     await $`git status`
