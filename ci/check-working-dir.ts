9c2696f6826e9ee2adb5b52fc2c4d2cce4ce1348
Piers Karsenbarg <piers.karsenbarg@gmail.com>
2024-04-01 10:36:20 +0100

    Added script to check working directory

---
+++ ci/check-working-dir.ts
@@ -0,0 +1,12 @@
+import { $ } from "bun";
+
+await $`git update-index -q --refresh`;
+
+const { stdout, stderr, exitCode } = await $`git diff-files`.quiet().nothrow();
+
+if (exitCode !== 0) {
+    console.log(`Working directory dirty.`);
+    await $`git status`
+    await $`git diff`
+    await $`exit 1`
+}
